e57b13d2220c9dd517e394a813c843f8614401c6
sofia <>
2020-01-25 02:13:34 +0100

    started code skeleton

---
+++ main.js
@@ -0,0 +1,21 @@
+/*TODO*/
+const turn = () => {
+	fall ()
+
+}
+
+/*TODO*/
+const fall = () => {
+	if ( /*free space below*/ ) {
+		/* lower piece */
+	} else {
+		/* finish move */
+	}
+}
+
+/* TODO
+* keyboard listener
+* crawly phase
+* counters
+
+*/
